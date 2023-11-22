06cad3a3364f2f5784ed967694f3b0de51d8ace0
jihyo3 <jgh6213@naver.com>
2023-11-22 07:34:56 +0000

    test

---
+++ chap9/prob6/execute1.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("parental process start\n");
+    if(fork()==0) {
+        execl("/bin/echo", "echo", "hello", NULL);
+        fprintf(stderr,"first fail");
+        exit(1);
+    }
+    printf("parental process end\n");
+}
+
