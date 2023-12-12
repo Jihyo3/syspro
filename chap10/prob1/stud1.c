326ad8e1379e6e325a8a130afb8896435d3bb632
“Jihyo3” <Jihyo3@github.com>
2023-12-13 03:42:37 +0900

    test

--- chap10/prob1/stud1.c
+++ chap10/prob1/stud1.c
@@ -2,37 +2,37 @@
 #include <stdlib.h>
 
 struct student {
-	int id;
-	char name[20];
+    int id;
+    char name[20];
 };
 
 int main() {
-	struct student *ptr;
-	int n,i;
+    struct student *ptr;
+    int n, i;
 
-	printf("How many students would you like to enter?\n");
-	scanf("%d ", &n);
-	if(n <= 0) {
-	fprintf(stderr, "Error: You entered the wrong number of students.\n");
-	fprintf(stderr, "Exit the program.\n");
-	exit(1);
-	}
+    printf("How many students would you like to enter?");
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        fprintf(stderr, "Error: You entered an invalid number of students.\n");
+        fprintf(stderr, "Exit the program.\n");
+        exit(1);
+    }
 
-	ptr = (struct student *) malloc(n * sizeof(struct student));
-	if(ptr == NULL) {
-		perror("malloc");
-		exit(2);
-	}
+    ptr = (struct student *)malloc(n * sizeof(struct student));
+    if (ptr == NULL) {
+        perror("malloc");
+        exit(2);
+    }
 
-	printf("%d Please enter your student number and name.\n", n);
-	for(i = 0; i < n; i++)
-		scanf("%d %s\n", &ptr[i].id, ptr[i].name);
+    printf("Please enter student numbers and names.\n");
+    for (i = 0; i < n; i++)
+        scanf("%d %s", &ptr[i].id, ptr[i].name);
 
-	printf("\n* Student Information (Reverse Order) *\n");
-	for(i = n-1; i >= 0; i--)
-		printf("%d %s\n", ptr[i].id, ptr[i].name);
+    printf("\n* Student Information (Reverse Order) *\n");
+    for (i = n - 1; i >= 0; i--)
+        printf("%d %s\n", ptr[i].id, ptr[i].name);
 
-	printf("\n");
-	exit(0);
+    free(ptr);
+
+    printf("\n");
+    return 0;
 }
-
